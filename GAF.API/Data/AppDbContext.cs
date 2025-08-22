923ea061a3b8388314244430cf3a37247afc0edb
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2025-08-22 11:37:05 -0300

    mtos erros amo

--- GAF.API/Data/AppDbContext.cs
+++ GAF.API/Data/AppDbContext.cs
@@ -4,15 +4,77 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GAF.API.Data;
 
-    public class AppDbContext : IdentityDbContext<User>
+public class AppDbContext : IdentityDbContext<User>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : 
-        base(options)
-        {
-        }
-
-        public DbSet<Transaction> Transactions { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Reports> Reports { get; set; }
-
     }
+
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Report> Reports { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        SeedDefaultUser(builder);
+    }
+
+    private static void SeedDefaultUser(ModelBuilder builder)
+    {
+        #region Populate Roles - Perfis de Usuário
+        List<IdentityRole> roles = new()
+        {
+            new IdentityRole() {
+               Id = "0b44ca04-f6b0-4a8f-a953-1f2330d30894",
+               Name = "Administrador",
+               NormalizedName = "ADMINISTRADOR"
+            },
+            new IdentityRole() {
+               Id = "bec71b05-8f3d-4849-88bb-0e8d518d2de8",
+               Name = "Funcionário",
+               NormalizedName = "FUNCIONÁRIO"
+            },
+            new IdentityRole() {
+               Id = "ddf093a6-6cb5-4ff7-9a64-83da34aee005",
+               Name = "Cliente",
+               NormalizedName = "CLIENTE"
+            },
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
+        #endregion
+
+        #region Populate Usuário
+        List<User> usuarios = new() {
+            new User(){
+                Id = "ddf093a6-6cb5-4ff7-9a64-83da34aee005",
+                Email = "gallojunior@gmail.com",
+                NormalizedEmail = "GALLOJUNIOR@GMAIL.COM",
+                UserName = "GalloJunior",
+                NormalizedUserName = "GALLOJUNIOR",
+                LockoutEnabled = true,
+                EmailConfirmed = true,
+                Nome = "José Antonio Gallo Junior"
+            }
+        };
+        foreach (var user in usuarios)
+        {
+            PasswordHasher<IdentityUser> pass = new();
+            user.PasswordHash = pass.HashPassword(user, "123456");
+        }
+        builder.Entity<Usuario>().HasData(usuarios);
+        #endregion
+
+        #region Populate UserRole - Usuário com Perfil
+        List<IdentityUserRole<string>> userRoles = new()
+        {
+            new IdentityUserRole<string>() {
+                UserId = usuarios[0].Id,
+                RoleId = roles[0].Id
+            }
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
+        #endregion
+    }
+
+}
